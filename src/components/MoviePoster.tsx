b68eca73d2a7ca55de695ae312d047c3c074bc48
francisco <francisco.urrea9@gmail.com>
2022-09-25 15:26:12 -0300

    getMovies => Promise.all + Obj => state

--- src/components/MoviePoster.tsx
+++ src/components/MoviePoster.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Image, StyleSheet, View} from 'react-native';
+import {useNavigation} from '@react-navigation/core';
 import {Movie} from '../interfaces/movieInterface';
 // import {REACT_APP_IMAGE_URL} from '@env';
 
@@ -11,6 +12,8 @@ interface Props {
 
 export const MoviePoster = ({movie, height = 420, width = 300}: Props) => {
   const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+  const navigation = useNavigation();
 
   return (
     <View
--- src/hooks/useMovies.tsx
+++ src/hooks/useMovies.tsx
@@ -1,24 +1,51 @@
 import {useEffect, useState} from 'react';
 import movieDB from '../api/movieDB';
-import {Movie, MovieDBNowPlaying} from '../interfaces/movieInterface';
+import {Movie, MovieDBResponse} from '../interfaces/movieInterface';
+
+interface MoviesState {
+  nowPlaying: Movie[];
+  popular: Movie[];
+  topRated: Movie[];
+  upcoming: Movie[];
+}
 
 export const useMovies = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [moviesNowPlaying, setMoviesNowPlaying] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<MoviesState>({
+    nowPlaying: [],
+    popular: [],
+    topRated: [],
+    upcoming: [],
+  });
 
   useEffect(() => {
     getMovies();
   }, []);
 
   const getMovies = async () => {
-    const {data} = await movieDB.get<MovieDBNowPlaying>('/now_playing');
+    const nowPlayingPromise = movieDB.get<MovieDBResponse>('/now_playing');
+    const popularPromise = movieDB.get<MovieDBResponse>('/popular');
+    const topRatedPromise = movieDB.get<MovieDBResponse>('/top_rated');
+    const upcomingPromise = movieDB.get<MovieDBResponse>('/upcoming');
 
-    setMoviesNowPlaying(data.results);
+    const response = await Promise.all([
+      nowPlayingPromise,
+      popularPromise,
+      topRatedPromise,
+      upcomingPromise,
+    ]);
+
+    setMovies({
+      nowPlaying: response[0].data.results,
+      popular: response[1].data.results,
+      topRated: response[2].data.results,
+      upcoming: response[3].data.results,
+    });
     setIsLoading(false);
   };
 
   return {
-    moviesNowPlaying,
+    ...movies,
     isLoading,
   };
 };
